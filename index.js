585798605b6178928187a41e588be6fd61a17c20
1wes <okemwawes@gmail.com>
2024-01-28 19:13:10 +0300

    define full endpoint url

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const { port } = require('./config/env-config');
+const { port, origin } = require('./config/env-config');
 
+const albums = require('./routes/albums');
+const users = require('./routes/users');
+const photos = require('./routes/photos');
+
+// global middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin:'localhost',
+    origin:origin,
     credentials:true
 }));
+
+app.use("/api", albums);
+app.use("/api", users);
+app.use("/api", photos);
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);

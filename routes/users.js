6254b946d539dfd5d0211e2231ef528e9c0888f0
1wes <okemwawes@gmail.com>
2024-02-01 14:59:29 +0300

    feat: finf user matching a specific provided id

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { users } = require('../mock-data/data');
+const { albums } = require('../mock-data/data');
 
 router.use((req, res, next) => {
     
@@ -11,6 +12,26 @@ router.use((req, res, next) => {
 router.get("/users", (req, res) => {
     
     res.send(users)
+});
+
+router.get("/users/:id/albums", (req, res) => {
+
+    var userId = parseInt(req.params.id);
+
+    const userAlbums = albums.filter(album =>
+        album.userId === userId);
+    
+    userAlbums ? res.send(userAlbums) : res.send([]);
+});
+
+router.get("/users/:userId", (req, res) => {
+    
+    var userId = parseInt(req.params.userId);
+
+    const user = users.find(user =>
+        user.id === userId);
+        
+    user ? res.send(user) : res.send("");
 })
 
 module.exports = router;

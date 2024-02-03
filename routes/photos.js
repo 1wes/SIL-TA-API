736303a3b0177aa90b4d1b848161bb0b40079af5
1wes <okemwawes@gmail.com>
2024-02-03 20:31:23 +0300

    modify photo title and save it

--- routes/photos.js
+++ routes/photos.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+const fs = require('fs');
+
+const path = require('path');
+
+const data = require('../mock-data/data');
+
 const { photos } = require('../mock-data/data');
+
+// function that modifies title
+const updateTitle = (array, id, newTitle) => {
+    
+    return array.map(item => (item.id === id ? { ...item, title: newTitle } : item));
+}
 
 router.use((req, res, next) => {
     
@@ -33,5 +45,27 @@ router.get("/photos/:id", (req, res) => {
 
     photo ? res.send(photo) : res.send("");
 });
+
+router.put("/photos/edit/:id", (req, res) => {
+    
+    const photoId = parseInt(req.params.id);
+
+    const newTitle = req.body.newTitle;
+    
+    data.photos = updateTitle(data.photos, photoId, newTitle);
+
+    saveNewTitle(data);
+})
+
+const saveNewTitle = (data) => {
+    
+    const fileContent = `module.exports=${JSON.stringify(data, null, 2)}`;
+
+    const rootPath = path.dirname(__dirname);
+
+    const filePath = path.join(rootPath, "mock-data", "data.js");
+    
+    fs.writeFileSync(filePath, fileContent, 'utf-8');
+}
 
 module.exports = router;
